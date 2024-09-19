1a35d203b4ea2436e5f15f82f63636300d50c670
Julia Giniyatullina <124470751+julia1gin@users.noreply.github.com>
2024-09-19 20:20:11 +0300

    Update activation_script.py

--- activation_script.py
+++ activation_script.py
@@ -3,7 +3,7 @@ import sys
 from urllib.request import urlopen
 from importlib.abc import PathEntryFinder
 from importlib.util import spec_from_loader
-
+import requests
 
 class URLFinder(PathEntryFinder):
     def __init__(self, url, available):
@@ -23,8 +23,18 @@ class URLFinder(PathEntryFinder):
 def url_hook(some_str):
     if not some_str.startswith(("http", "https")):
         raise ImportError
-    with urlopen(some_str) as page:  # requests.get()
-        data = page.read().decode("utf-8")
+    """with urlopen(some_str) as page:  # requests.get()
+        data = page.read().decode("utf-8")"""
+
+    try:
+        response = requests.get(some_str)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        raise ImportError(f"Не удалось получить модуль с {some_str}. Ошибка: {e}")
+
+    response1 = requests.get(some_str)
+    data = response1.text
+
     filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
     modnames = {name[:-3] for name in filenames}
     return URLFinder(some_str, modnames)
@@ -37,7 +47,17 @@ class URLLoader:
         return None
 
     def exec_module(self, module):
-        with urlopen(module.__spec__.origin) as page:
-            source = page.read()
-        code = compile(source, module.__spec__.origin, mode="exec")
+        """with urlopen(module.__spec__.origin) as page:
+            source = page.read()"""
+        response1 = requests.get(module.__spec__.origin)
+
+        try:
+            url = module.__spec__.origin
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise ImportError(f"Не удалось выполнить модуль {module.__name__}. Ошибка: {e}")
+
+        data = response1.text
+        code = compile(data, module.__spec__.origin, mode="exec")
         exec(code, module.__dict__)
